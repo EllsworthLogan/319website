be51d6a035c696e07ab59104e64e915bc3fbd221
prolling <prolling@iastate.edu>
2023-03-08 21:46:17 -0600

    many changes

--- main.js
+++ main.js
@@ -3,6 +3,27 @@ function toggleMobileMenu(menu){
   menu.classList.toggle('open'); 
 }
 
+//for div links
+    // Get all the clickable divs
+    const clickableDivs = document.querySelectorAll('[data-href]');
+    
+    document.addEventListener('DOMContentLoaded', function() {
+      // Select all clickable divs
+      const clickableDivs = document.querySelectorAll('[data-href]');
+      
+      // Add a click event listener to each one
+      clickableDivs.forEach(div => {
+        div.addEventListener('click', () => {
+          // Get the URL from the data-href attribute
+          const url = div.getAttribute('data-href');
+            
+          // Redirect the user to the URL
+          window.location.href = url;
+        });
+      });
+    });
+
+//popuate the merch page
 
 
 
